50ffd7a7195739bae32e12ec76972a941487d18e
Andrew Nguyen <andrewnguyen14@gmail.com>
2020-06-01 21:42:57 -0700

    add to block to template

--- app/code/FNL/Address/Block/Address.php
+++ app/code/FNL/Address/Block/Address.php
@@ -2,11 +2,6 @@
 namespace FNL\Address\Block;
 class Address extends \Magento\Framework\View\Element\Template
 {
-	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
-	{
-		parent::__construct($context);
-	}
-
 	public function sayHello()
 	{
 		return __('Hello World');
