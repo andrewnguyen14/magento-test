71f141db989e3db40f752524cc6d870af923f34e
Andrew Nguyen <andrewnguyen14@gmail.com>
2020-06-02 20:45:05 -0700

    move get all function into address block

--- app/code/FNL/Address/Block/Address.php
+++ app/code/FNL/Address/Block/Address.php
@@ -2,8 +2,18 @@
 namespace FNL\Address\Block;
 class Address extends \Magento\Framework\View\Element\Template
 {
-	public function sayHello()
-	{
-		return __('Hello World');
-	}
+	// public function sayHello()
+	// {
+	// 	return __('Hello World');
+	// }
+	public function getAllOptions() {
+		if ($this->_options === null) {
+				$this->_options = [
+						['label' => __('--Select--'), 'value' => ''],
+						['label' => __('Residential'), 'value' => 'Residential'],
+						['label' => __('Business'), 'value' => 'Business']
+				];
+		}
+		return $this->_options;
+}
 }
