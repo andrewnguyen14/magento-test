7f81a383d157883d477aeb3eee2e87066327b0db
Andrew Nguyen <andrewnguyen14@gmail.com>
2020-06-12 16:04:26 -0700

    update fedex schema

--- app/code/FNL/FedexImport/Setup/InstallSchema.php
+++ app/code/FNL/FedexImport/Setup/InstallSchema.php
@@ -1,89 +1,93 @@
 <?php
+  
 namespace FNL\FedexImport\Setup;
-
-class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
+  
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+  
+class InstallSchema implements InstallSchemaInterface
 {
-
-	public function install(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
-	{
-		$installer = $setup;
-		$installer->startSetup();
-		if (!$installer->tableExists('fedex')) {
-			$table = $installer->getConnection()->newTable(
-				$installer->getTable('fedex')
-			)
-			->addColumn(
-        'comment_id',
-        \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-        null,
-        [
-            'identity' => true,
-            'nullable' => false,
-            'primary'  => true,
-            'unsigned' => true,
-        ],
-        'ID'
-    )
-    ->addColumn(
-        'name',
-        \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-        255,
-        ['nullable => false'],
-        'Name'
-    )
-    ->addColumn(
-        'email',
-        \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-        255,
-        [],
-        'Email'
-    )
-    ->addColumn(
-        'url_key',
-        \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-        255,
-        [],
-        'URL Key'
-    )
-    ->addColumn(
-        'message',
-        \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-        '18K',
-        [],
-        'Message'
-    )
-    ->addColumn(
-        'status',
-        \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-        1,
-        [],
-        'Comment Status'
-    )
-    ->addColumn(
-        'created_at',
-        \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-        null,
-        ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-        'Created At'
-    )->addColumn(
-        'updated_at',
-        \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-        null,
-        ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
-        'Updated At')
-				->setComment('Fedex Table');
-			$installer->getConnection()->createTable($table);
-			$installer->getConnection()->addIndex(
-				$installer->getTable('fedex'),
-				$setup->getIdxName(
-					$installer->getTable('fedex'),
-					['name', 'email', 'url_key', 'message'],
-					\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
-				),
-				['name', 'email', 'url_key', 'message'],
-				\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
-			);
-		}
-		$installer->endSetup();
-	}
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        if (!$installer->tableExists('fedex')) {
+            $table = $installer->getConnection()
+                     ->newTable($installer->getTable('fedex'))
+                     ->addColumn(
+                         'fedex_id',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                         null,
+                         [
+                             'identity' => true,
+                             'nullable' => false,
+                             'primary'  => true,
+                             'unsigned' => true,
+                         ],
+                         'ID'
+                     )
+                     ->addColumn(
+                         'name',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                         255,
+                         ['nullable => false'],
+                         'Name'
+                     )
+                     ->addColumn(
+                         'email',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                         255,
+                         [],
+                         'Email'
+                     )
+                     ->addColumn(
+                         'url_key',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                         255,
+                         [],
+                         'URL Key'
+                     )
+                     ->addColumn(
+                         'message',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                         '18K',
+                         [],
+                         'Message'
+                     )
+                     ->addColumn(
+                         'status',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                         1,
+                         [],
+                         'Fedex Status'
+                     )
+                     ->addColumn(
+                         'created_at',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                         null,
+                         ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+                         'Created At'
+                     )->addColumn(
+                         'updated_at',
+                         \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                         null,
+                         ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                         'Updated At')
+                     ->setComment('fedex Table');
+            $installer->getConnection()->createTable($table);
+  
+            $installer->getConnection()->addIndex(
+                $installer->getTable('fedex'),
+                $setup->getIdxName(
+                    $installer->getTable('fedex'),
+                    ['name', 'email', 'url_key', 'message'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+                ),
+                ['name', 'email', 'url_key', 'message'],
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+        }
+        $installer->endSetup();
+    }
 }
